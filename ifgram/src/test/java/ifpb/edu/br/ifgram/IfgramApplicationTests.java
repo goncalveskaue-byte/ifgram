54834d560269fec7a7eb27820a268ab98d1de0b7
Francisco Kauê de Sousa Gonçalves <202412060036@ad.ifpb.edu.br>
2026-08-27 08:35:31 -0300

    primeiro commit

---
+++ ifgram/src/test/java/ifpb/edu/br/ifgram/IfgramApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpb.edu.br.ifgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
