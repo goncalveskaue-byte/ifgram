a1134f88e7d550520a2d00368e1aee08ca7d761d
Francisco Kauê de Sousa Gonçalves <202412060036@ad.ifpb.edu.br>
2026-09-03 08:37:07 -0300

    adicionei user controller

--- ifgram/src/main/java/ifpb/edu/br/ifgram/Controller/UsersController.java
+++ ifgram/src/main/java/ifpb/edu/br/ifgram/Controller/UsersController.java
@@ -1,4 +1,15 @@
 package ifpb.edu.br.ifgram.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
 public class UsersController {
+
+    @GetMapping
+    public String getUser () {
+        return "get user was called";
+    }
 }
