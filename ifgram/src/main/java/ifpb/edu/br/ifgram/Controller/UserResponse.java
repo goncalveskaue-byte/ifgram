c64d0f6d05c4bd0378f8d43b9addd09ddf0cda0c
Francisco Kauê de Sousa Gonçalves <202412060036@ad.ifpb.edu.br>
2026-09-23 09:56:50 -0300

    UserResponse

---
+++ ifgram/src/main/java/ifpb/edu/br/ifgram/Controller/UserResponse.java
@@ -0,0 +1,9 @@
+package ifpb.edu.br.ifgram.Controller;
+
+public class UserResponse ( Long Id,String Nome, String Email){
+public static UserResponse from(User user) {
+    return new UserResponse(user.getId(), user.getNome(), user.getEmail())
+
+}
+}
+
